1a7db3b77856505a31f46bae2a14e79086140805
Joost Voskuil <joost.voskuil@navara.nl>
2025-03-31 17:42:28 +0200

    Rename test functions to reflect correct parameter usage in GET requests

--- exercisefiles/python/test_server.py
+++ exercisefiles/python/test_server.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
 
-def test_key_not_passed():
+def test_name_not_passed():
     response = requests.get('http://localhost:3000/hello')
     assert response.text == 'name not passed'
