8472536586ddb12a92cbea235032b64dc86c32f8
Joost Voskuil <joost.voskuil@navara.nl>
2025-05-08 08:08:49 +0200

    Add comments again for the springboot excersize 1 that were accedentely removed

--- exercisefiles/java/springboot/src/main/java/copilot/CopilotDemoApplication.java
+++ exercisefiles/java/springboot/src/main/java/copilot/CopilotDemoApplication.java
@@ -1,5 +1,13 @@
 package copilot;
 
+// write a java sprintboot http server that will expose a method call "hello" that will return the value of the name passed in the query string
+// example: http://localhost:3000/hello?name=world
+// if the name is not passed, return "name not passed"
+// if the name is passed, return "hello " + key
+// if the url has other methods, return "method not implemented"
+// when server is listening, log "server is listening on port 3000"
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
